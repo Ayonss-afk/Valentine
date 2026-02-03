57e5a76eecdba2868d713f23281b514e0984260f
Ayonss-afk <ayanshprasad20@gmail.com>
2026-02-03 23:32:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,53 +5,53 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Ananya",
+    valentineName: "Tanvi",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Wanna Be My Valentine? 💝",
+    pageTitle: "Wanna Gimme A Chokolate? 💝",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
-        hearts: ['❤️', '💖', '💝', '💗', '💓'],  // Heart emojis
-        bears: ['🧸', '🐻']                       // Cute bear emojis
+        hearts: ['🍪', '🍫', '🍫', '🍫', '🍪'],  // Heart emojis
+        bears: ['🍩', '💕']                       // Cute bear emojis
     },
 
     // Questions and answers
     // Customize each question and its possible responses
     questions: {
         first: {
-            text: "Do you lick me?",                                    // First interaction
+            text: "Am I Chokolate Deservable?",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "I don't like you, I Luv you! 💕"           // Secret hover message
+            secretAnswer: "Arre Ha Ha Sure!💕"           // Secret hover message
         },
         second: {
-            text: "How much do you luv me?",                          // For the love meter
+            text: "How Much Chances For Me Chokolate?",                          // For the love meter
             startText: "This much!",                                   // Text before the percentage
             nextBtn: "Yayayya...And"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2026? 🌹", // The big question!
-            yesBtn: "Yes Bbg!!",                                             // Text for "Yes" button
-            noBtn: "Neh..I no Like u"                                                 // Text for "No" button
+            text: "So When Me Giving My Chokolate? 🌹", // The big question!
+            yesBtn: "The Next Time We Meet!!",                                             // Text for "Yes" button
+            noBtn: "Very Soon..Yayyy"                                                 // Text for "No" button
         }
     },
 
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "Itta Saara Pyaaar?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "Oye Hoye Hoye Hoye! 🚀💝",              // Shows when they go past 1000%
-        normal: "Less Gooooo! 🥰"                           // Shows when they go past 100%
+        extreme: "Dairy Milk????? 🥰🚀💝",  // Shows when they go past 5000%
+        high: "5 Star?? 🚀💝",              // Shows when they go past 1000%
+        normal: "Ferero Rocher!!!🥰"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Oh My Gawd I love You Sooooo Much Baby! 🎉💝💖💝💓",
-        message: "Now Lemme Come Over And Give You The Best Gift Ever!",
-        emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
+        title: "YAAAAAAAAAAAAAAAYYYYYYY!🍩🍪💕🍫 ",
+        message: "Thanks For The Chokolate In Advance!",
+        emojis: "🍩🍫🍪🍩🍪🍫"  // These will bounce around
     },
 
     // Color scheme for the website
