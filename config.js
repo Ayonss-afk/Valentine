db244913b38017fd4949e4e116ba83b91eb10d6b
Ayonss-afk <ayanshprasad20@gmail.com>
2026-02-03 14:46:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ const CONFIG = {
     // Customize each question and its possible responses
     questions: {
         first: {
-            text: "Do you lickₗᵢₖₑ me?",                                    // First interaction
+            text: "Do you lick me?",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
             secretAnswer: "I don't like you, I Luv you! 💕"           // Secret hover message
